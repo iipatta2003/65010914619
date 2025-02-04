68c77f627c02b3f7f9be5da2e3e82d60e0b07aba
iipatta2003 <65010914619@msu.ac.th>
2025-02-04 12:07:43 +0700

    3

--- i/connectdb.php
+++ i/connectdb.php
@@ -9,8 +9,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "qq123456";
+$db = "shop4619";
 
 $conn = mysqli_connect($host,$user,$pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_select_db($conn,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
